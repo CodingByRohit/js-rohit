4e712299ca32fa7635c8e671496c071cd2cca48f
developer420-coder <fullstackdeveloper512@gmail.com>
2025-05-26 17:31:35 +0000

    datatypes

---
+++ database.js
@@ -0,0 +1,12 @@
+// alert(3+3)    we are using nodejs , not browser
+
+console.log(3+3) 
+console.log("Rohit")
+
+let name = "rohit"
+let age = 18
+let isLoggedIn = false
+
+console.log(typeof "rohit")
+console.log(typeof null)
+console.log(typeof undefined)
