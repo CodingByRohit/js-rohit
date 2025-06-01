c1cee4a435fd40067721319c1f15724431e7db32
developer420-coder <fullstackdeveloper512@gmail.com>
2025-06-01 15:53:17 +0000

    datatypes summary

--- datatype-summary.js
+++ datatype-summary.js
@@ -3,7 +3,35 @@
 //  7 types : String, Number, Null, Undefined, BigInt, Boolean, Symbol
 
 
+    const score = 100
+    const scoreValue = 100.3
+    const isLoggedIn = false
+    const outSideTemp = null
+    let userEmail;
+
+    const id = Symbol('123')
+    const anotherId = Symbol('123')
+
+    // console.log(id === anotherId)
+
+    const bigNumber = 6546231346646464321
+
+
+
+
 // Reference (Non Premitive)
 
 // Array , Objects, Functions
 
+const heros = ["Shaktiman", "Doremon", "Ironman"];
+
+const myObj = {
+    name : "John",
+    age : 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof myFunction)
