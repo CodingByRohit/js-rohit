4cb4a672c72eb1da8e68a8b4a05b9f6f130c11dd
Rohit Coder <fullstackdeveloper512@gmail.com>
2025-06-12 17:22:11 +0000

    arrray concepts

---
+++ basics/array.js
@@ -0,0 +1,16 @@
+const myArr = [0,1,2,3,4,5]
+const myHeros = ['shaktiman', "nagarjun"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[1]);
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(0)
+// myArr.unshift(9)
+// myArr.shift()
+
+
+console.log(myArr)
